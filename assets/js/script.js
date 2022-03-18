97422ae47eb61af1ab0c3a1ea0f1d706d5cfda90
Sophia Davis <sophiadavis@Sophias-MBP.attlocal.net>
2022-03-18 14:28:18 -0400

    working on breed api and selection boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 function getApi() {
 
-    var requestUrl = "https://api.petfinder.com/v2/animals";
+    var requestUrl = "https://api.petfinder.com/v2/types/{type}/breeds";
   
     fetch(requestUrl, { 
       method: "GET", 
       headers: new Headers({
-        "Authorization": "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJveE5rRDV6MWlmWklWazYwQTlUcEdEdnc5V2ZIVDlLN3lhb2p6YkFISXZkWlFtYW9rMiIsImp0aSI6IjMyZmY3NDE1YTJhY2I1OWRlNTc3NTM0MTBhOWNhYTI5MjM5MzNhZDMzMDRlZDc4MjE0NTYyYWRjNmZjYTBhY2RmMzAxMTU2MGYyMjNiOTQxIiwiaWF0IjoxNjQ3NDcxMjM4LCJuYmYiOjE2NDc0NzEyMzgsImV4cCI6MTY0NzQ3NDgzOCwic3ViIjoiIiwic2NvcGVzIjpbXX0.GCwp6AzGxHtMaH7mKC4uqDiv0gSWDGXcItXf-qxRn3df6zGhSjqS3Rs3zzTsw5oEaAU6hca1lTmOVqiweFFsEzKJOXH8y3bPFlCfaEZdeJ6kAAgIyleFSvanAghAQMJYhTty9ZEKWk1lMyPV6PavGSwZcBA5jC2h5L0ivhE3TS4Kaft3IBJLQaMILg8qxA1cRyuWfoW3aD_0hrujOUYnJMm9XMCGk978bEXMIt_Piqa90ojR1QB9OpgLfRiMvpTn8HK5Dsn7Yr2Z6uxxwsnBcFEWjrrvCGVlcXxQaBhvR4JJOCkzW5NXuSiWQJWf75AZE0imgqKNBNklzc-7lQjh6A", 
+        "Authorization": "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJveE5rRDV6MWlmWklWazYwQTlUcEdEdnc5V2ZIVDlLN3lhb2p6YkFISXZkWlFtYW9rMiIsImp0aSI6IjNkNDhjYzczYWYzMjJjNTFiZDk1ZDU2NjQ3MmM3ZGJiODgzYmE3OTQwZDJmNzgzMDI4NDRmNzcxMDcyZmIzMDI2YTFmZTA1MGUyMTRkMWVlIiwiaWF0IjoxNjQ3NjI0NTA4LCJuYmYiOjE2NDc2MjQ1MDgsImV4cCI6MTY0NzYyODEwOCwic3ViIjoiIiwic2NvcGVzIjpbXX0.ZXDKtlqA_2sCw_2u3t3QVNJ7_0kNNj7sa8ugQVbs0UC8TIQqAFSxm3UYcP7hY56aEexZBHJ20gmfsDbsWTsR2KlbY-bt2fzQuyii-vu4s-iN-htUcjs-RvlCfNNA1PHvIDYIVBWEZY09PvnxhjoQTLXuvMV1smQBfzdkx9y0_sYIzY_DT4K1rNw0F1NxiYIAdJW4DZCntyAuzYtPggqDgSNrVfgi_BPBFlLAvlfp2DZUFNxsPJzviKgqUMz0-yWfDEtFdh5-brr8zglwqef6sfWph9uX-iTNCsDLstTpKjkcsZY250988mQTi3MjEYBJgGzMjd_eS0Pqc5V_Hjxnkg", 
       })
     })
       .then(function(response) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
   content: [
     "./assets/**/*.{html,js}",
-<<<<<<< HEAD
     "./*.html"
-=======
-    "./*.html",
->>>>>>> 7696934fdb28b2544318448b7948b1feccab357b
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+GET https://api.petfinder.com/v2/types/{type}/breeds
