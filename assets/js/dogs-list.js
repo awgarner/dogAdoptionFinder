ee93c8480e7783314b1554717e2e87542b10b93a
emilyk <emily.g.kruse@gmail.com>
2022-03-20 00:38:04 -0400

    display dog images

--- assets/js/dogs-list.js
+++ assets/js/dogs-list.js
@@ -16,7 +16,7 @@ function getAdoptionList() {
   fetch(requestUrl, { 
     method: "GET", 
     headers: new Headers({
-      "Authorization": "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJveE5rRDV6MWlmWklWazYwQTlUcEdEdnc5V2ZIVDlLN3lhb2p6YkFISXZkWlFtYW9rMiIsImp0aSI6ImYzNTNlOTU5MzYwNGViMTA4MGY2YTY4MDg2NTA2NDAyYTc0NzY2ODUzMDk5NWNkMDIwNDc0YWU1YTgxZTU2NjJlNTI3ZDEwNmM4NThjYjQzIiwiaWF0IjoxNjQ3NzQyOTY5LCJuYmYiOjE2NDc3NDI5NjksImV4cCI6MTY0Nzc0NjU2OSwic3ViIjoiIiwic2NvcGVzIjpbXX0.kYHoCRejVwZVpc3q4FNI_sgip61EI7BLcV5jfAdjafnRpd0SoEcTl3WxCEENbxevAQOQCMfEoBuOo07dcKvYRmzABQdctCUz-GHr-Eov1Msu0OO7Prssh9oIpIH8EWmslZGcpKhdMM_oOXKWReabrMX3KG4_wQR8tGyMi0UHZlHQauUo18ffY8f4VKwIWz8iE3OjShSKinr70QNMRSWGvuqNAOiJOni75UDXhQKo1JnA_r8lf-rxB_DqvE0OGqWTWqe2s6HIbJLmqxy4OM_a4-E1WxXAIGE6Yl_YvgWCG_nUBDbGpIU-l1_21jyvdct9cF5PmApg-LQX84nhMM8wMQ", 
+      "Authorization": "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJveE5rRDV6MWlmWklWazYwQTlUcEdEdnc5V2ZIVDlLN3lhb2p6YkFISXZkWlFtYW9rMiIsImp0aSI6IjBiZTJlNzBkOGY0MmIwM2EyZWRmZTNkYThkZGNlZDgzNTQ3NWRiNjgwNWU2NWZjYmU3NzE2YjM1M2VmNDdjZGY3YWUzNmVlMWU1NGUwMTY4IiwiaWF0IjoxNjQ3NzUwNjI5LCJuYmYiOjE2NDc3NTA2MjksImV4cCI6MTY0Nzc1NDIyOSwic3ViIjoiIiwic2NvcGVzIjpbXX0.se0gSpYXQAF-zdN4ME1-m3CnYRWnvATfidlcYxk7eDo6QPtXJ6761JKqJF-MnQ3dt2gLIeKycbDEUS_Uyv0Q5JSapzzI00zzbT4pBhYA4W7OfcOqZ4KRpE4vSyvjexF7LgUgbw1b7E8hy-phBH58ryMLDdKi8KWBVsJIW_K2htWhx8-J6QgGkAThtD_hAh3wL0W824NVIvsdUkfAX8de9n1rwkSxh_GRLClavADX48jPucq4t5Yu-qLytf4UParB-V0teqk8bUmMHWF2MNwrjART6M95qsGnc2LiCeKUeXQnaTgCPUpPkGjzw8rRwxIGBE7wGxTqmT1mivGwS54GdA", 
     })
   })
     .then(function(response) {
@@ -42,8 +42,27 @@ let displayDogs = function(dogs) {
   }
   // loop over adoptable dogs
   for (let i = 0; i < dogs.animals.length; i++) {
-    console.log(dogs.animals[i].name);
+    // define dog id
+    let dogId = dogs.animals[i].id;
+    // create a link element container for each dog
+    let dogEl = document.createElement("a");
+    dogEl.classList = "flex max-w-full rounded overflow-hidden shadow-lg align-middle";
+    dogEl.setAttribute("href", "./page3.html?dog=" + dogId);
+
+    // define image url
+    let dogImageUrl = dogs.animals[i].primary_photo_cropped.full;
+    let urlEnding = dogImageUrl.split("=")[1];
+    console.log(dogImageUrl, urlEnding);
+    // create div for image of dog to display
+    let dogImageEl = document.createElement("div");
+    dogImageEl.classList = "flex items-center px-6 py-4";
+    dogImageEl.innerHTML = "<img src='https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/" + dogId + "/1/?bust=" + urlEnding + "' alt='dogs.animals[i].name' />";
+
+    dogEl.appendChild(dogImageEl);
+
+    dogsContainerEl.appendChild(dogEl);
   }
+
 }
 
 
