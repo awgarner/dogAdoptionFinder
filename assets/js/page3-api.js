d5efac1908e9d8d526f0a25e5585b17b40c34828
Trent Dodson <dodson-7-7@outlook.com>
2022-03-24 20:51:44 -0400

    added email and phone to fucntion display

--- assets/js/page3-api.js
+++ assets/js/page3-api.js
@@ -14,7 +14,7 @@ function getAdoptionInfo(dogId) {
   fetch(requestUrl, { 
     method: "GET", 
     headers: new Headers({
-      "Authorization": "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJveE5rRDV6MWlmWklWazYwQTlUcEdEdnc5V2ZIVDlLN3lhb2p6YkFISXZkWlFtYW9rMiIsImp0aSI6IjdiM2I3YjRjMjhmNTZkMTU0MWFhOTRhMjRlN2IwMzkwODVmNTU0MzZkMDQyNDRkNTdmNzgwNDdlYTc3NTM0NTRhODcxYTUyNzY5NDFjMjY2IiwiaWF0IjoxNjQ4MDgxNjg4LCJuYmYiOjE2NDgwODE2ODgsImV4cCI6MTY0ODA4NTI4OCwic3ViIjoiIiwic2NvcGVzIjpbXX0.gpBdewdLhaUyaA-iGhOcXWa0LtxdPikVskenl_j_sWShjmYY-bQ7aAMAMIqAz8U5DJXQ89RfUbJVzhK4GLQqlrZdUuA4rwpMRpRoHq3rkf026Kjaie3VoZE8CWpZUzSDYlJhSgaZvu0ZULmjvXDKObL-534nM8xgEG4VNU90iFOBpyi6RWAo0kySkDuwXZcrJuuCrM9DUqRIHRPGCuFv7oc5Uq8c4mRM9-5p4FDuQRatbfls26v7EyHt1RVe8sWwRRk-bQ7O8cBXzI5PiDiYuXdEn6tjOA6ACqZTy1w4SZLhbXweF9zU_1uMEqMpkUZeO9Sirpp15mWmAEhhez-qAQ", 
+      "Authorization": "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJveE5rRDV6MWlmWklWazYwQTlUcEdEdnc5V2ZIVDlLN3lhb2p6YkFISXZkWlFtYW9rMiIsImp0aSI6ImYwZGMwZWRlY2JjMTVkOTljNzkyNmVlZTRlMjNmYjI1YTkzNzU4ZjkwNzIxYmQ5NWVmYmZmNjlkMmExNzQ1YWZjNWVkNGJiNTAxYTJiZThlIiwiaWF0IjoxNjQ4MTY1NTkyLCJuYmYiOjE2NDgxNjU1OTIsImV4cCI6MTY0ODE2OTE5Miwic3ViIjoiIiwic2NvcGVzIjpbXX0.EpdOra8qzASP4CFT-amec46RYhVVJsGevifx23-_vE1ko4TAq34yfF1mmblD83RB4nDyZsdpgR-ELgV6t5f8RcJywgwyuyDs29c-COizPehN2LeUXXV1leSrGhKFbA62Z1aQeHfSwpI24GokaYUgqpL4bCNIuKBdm0y8bb6z9MXgDP5mRzW0BrRViDUMe94gTQJWldImi3-2xjEkdwJdGWAr0eO-03zdLVp-ntJLo3GUZ_by6hFpbvC32b1IZiWxCVfb2qLOzfTWMGC0lY_bFr236KzXV1ph3fTNKAXQYNx-_k5Nx3d7pYmRODQEqR9iJGPpufS2xTFKOyiNPxvBhQ", 
     })
   })
     .then(function(response) {
@@ -39,6 +39,10 @@ function displayDogInfo(data) {
   var state = data.animal.contact.address.state;
   var zip = data.animal.contact.address.postcode;
   document.querySelector("#address2").textContent = city + ", " + state + " " + zip;
+  var email = data.animal.contact.email;
+  document.querySelector("#email").textContent = email;
+  var phone = data.animal.contact.phone;
+  document.querySelector("#phone").textContent = phone;
 }
 
 //getAdoptionInfo();
@@ -64,3 +68,36 @@ function getBreedInfo() {
   
   //getBreedInfo();
   getDogId();
+
+  //local storage
+  //empty array for saved dogs
+  let savedDogs =[]
+  //container needed to hold dog cards
+  let dogContainerEl = document.getElementById("dogCards")
+
+  //itterate api response
+  for(i=0; i<savedDogs.length; i++) {
+    //button needed
+    var saveBtn = document.createElement("button")
+    //Need to set inner text of button
+
+    //set button value to current dog
+    //needs to be a string. can not set value to an object
+    saveBtn.value = JSON.stringify(dogs[i])
+
+
+    saveBtn.addEventListener("click", function(event){
+      event.preventDefault()
+      //pushing this dog to array
+      //need to parse the buttons value because of using stringify earlier
+      savedDogs.push(JSON.parse(event.target.value))
+      //saving array to local needs stringify
+      localStorage.setItem("savedDogs", JSON.stringify(savedDogs))
+    })
+
+    //create other elements and div for dog card
+    //append the pieces to dog card
+
+    //then append dog card to page
+    dogContainerEl.append(dogCard)
+  }
